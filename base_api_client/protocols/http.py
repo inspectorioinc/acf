70b9e06af9b3fedf737e7fb217cbae7e240f60a9
Yuriy Sahalovich <yuriy@inspectorio.com>
2018-05-30 18:18:52 +0300

    Minor updates for the initial HTTP protocol implementation

--- base_api_client/protocols/http.py
+++ base_api_client/protocols/http.py
@@ -6,4 +6,17 @@ from base_api_client.protocols.base import BaseProtocol
 class HttpProtocol(BaseProtocol):
 
     def execute(self, method, url, *args, **kwargs):
+        self._authenticate(*args, **kwargs)
+        try:
+            return self._perform_request(method, url, *args, **kwargs)
+        except Exception as e:
+            return self._handle_error(method, url, *args, **kwargs)
+
+    def _authenticate(self, *args, **kwargs):
+        pass
+
+    def _perform_request(self, method, url, *args, **kwargs):
         return requests.request(method, url, **kwargs)
+
+    def _handle_error(self, error, *args, **kwargs):
+        pass

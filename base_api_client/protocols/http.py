78953721970f16d20eb84301eb940b4bde00039d
Yuriy Sahalovich <yuriy@inspectorio.com>
2018-05-31 18:18:28 +0300

    Implement base error handling logic for the HTTP protocol

--- base_api_client/protocols/http.py
+++ base_api_client/protocols/http.py
@@ -1,22 +1,22 @@
 import requests
 
 from base_api_client.protocols.base import BaseProtocol
+from base_api_client.errors.http import RequestError
 
 
 class HttpProtocol(BaseProtocol):
 
-    def execute(self, method, url, *args, **kwargs):
-        self._authenticate(*args, **kwargs)
+    def execute(self, *args, **kwargs):
         try:
-            return self._perform_request(method, url, *args, **kwargs)
-        except Exception as e:
-            return self._handle_error(method, url, *args, **kwargs)
-
-    def _authenticate(self, *args, **kwargs):
-        pass
+            return self._perform_request(*args, **kwargs)
+        except Exception as error:
+            return self._handle_error(error, *args, **kwargs)
 
     def _perform_request(self, method, url, *args, **kwargs):
         return requests.request(method, url, **kwargs)
 
     def _handle_error(self, error, *args, **kwargs):
-        pass
+        raise RequestError(
+            message=getattr(error, 'message'),
+            base_error=error
+        )

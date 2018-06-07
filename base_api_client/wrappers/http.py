601feb66b99ef7000ef10bbf0215d0adbaa03025
Yuriy Sahalovich <yuriy@inspectorio.com>
2018-06-07 12:59:29 +0300

    Add `is_successful` flag to the HTTP result container

--- base_api_client/wrappers/http.py
+++ base_api_client/wrappers/http.py
@@ -1,6 +1,8 @@
 from cached_property import cached_property
 
-from base_api_client.wrappers.base import BaseResultWrapper, BaseParamsWrapper
+from base_api_client.wrappers.base import (
+    BaseParamsWrapper, BaseResultContainer, BaseResultWrapper
+)
 from base_api_client.errors.http import ParamsError, ResultError
 
 
@@ -36,13 +38,28 @@ class HttpParamsWrapper(BaseParamsWrapper):
         pass
 
 
+class HttpResultContainer(BaseResultContainer):
+
+    def __init__(
+        self, is_successful=True, parsed_result=None, raw_result=None
+    ):
+        super(HttpResultContainer, self).__init__(
+            parsed_result=parsed_result, raw_result=raw_result
+        )
+        self.is_successful = is_successful
+
+
 class HttpResultWrapper(BaseResultWrapper):
+
+    class Meta:
+        container = HttpResultContainer
 
     @cached_property
     def wrapped(self):
         try:
             return self.Meta.container(
-                parsed_result=self.parsed_result,
+                is_successful=self._is_successful,
+                parsed_result=self._parsed_result,
                 raw_result=self.raw_result
             )
         except Exception as error:
@@ -51,6 +68,10 @@ class HttpResultWrapper(BaseResultWrapper):
                 base_error=error
             )
 
-    @cached_property
-    def parsed_result(self):
+    @property
+    def _is_successful(self):
+        return True
+
+    @property
+    def _parsed_result(self):
         raise NotImplementedError()

c2e9743ab788f95afd9cae3dcff3b1615a693935
Yuriy Sahalovich <yuriy@inspectorio.com>
2018-06-08 14:52:09 +0300

    Add error message to the string representation of the error object

--- base_api_client/errors/base.py
+++ base_api_client/errors/base.py
@@ -3,6 +3,9 @@ class BaseError(Exception):
     def __init__(self, message, *args, **kwargs):
         self.message = message
 
+    def __str__(self):
+        return self.message
+
 
 class UnknownResourceError(BaseError):
     pass

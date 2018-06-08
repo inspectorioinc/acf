fa1a7cf038d0884211de7c05ce58c543fe21ed73
Yuriy Sahalovich <yuriy@inspectorio.com>
2018-06-08 16:38:33 +0300

    Implement example client library for getting current time from the remote API

---
+++ example/time_api_client/wrappers/time.py
@@ -0,0 +1,25 @@
+import json
+
+from base_api_client.wrappers.http import HttpParamsWrapper, HttpResultWrapper
+
+
+class TimeParamsWrapper(HttpParamsWrapper):
+
+    METHOD = 'GET'
+    URL_TEMPLATE = 'http://worldclockapi.com/api/json/{timezone}/now'
+    DEFAULT_TIMEZONE = 'utc'
+
+    def build_url(self):
+        return self.URL_TEMPLATE.format(
+            timezone=self.raw_kwargs.get('timezone', self.DEFAULT_TIMEZONE)
+        )
+
+    def build_method(self):
+        return self.METHOD
+
+
+class TimeResultWrapper(HttpResultWrapper):
+
+    @property
+    def _parsed_result(self):
+        return json.loads(self.raw_result.content)['currentDateTime']

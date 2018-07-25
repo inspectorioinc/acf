bf601414a665754537e574acb5c5fbcde87b1e3d
Aliaksei Urbanski <aliaksei@inspectorio.com>
2018-07-25 11:27:24 +0300

    Use @pytest.mark.parametrize for test_defined

--- base_api_client/tests/test_constants.py
+++ base_api_client/tests/test_constants.py
@@ -1,3 +1,5 @@
+import pytest
+
 from base_api_client.constants import NOT_SET, defined
 
 
@@ -7,7 +9,9 @@ class TestConstants(object):
         assert bool(NOT_SET) is False
         assert str(NOT_SET) == 'NotSet'
 
-    def test_defined(self):
+    def test_not_defined(self):
         assert not defined(NOT_SET)
-        for value in (None, 0, '', [], ()):
-            assert defined(value)
+
+    @pytest.mark.parametrize('value', (None, 0, '', [], (), set()))
+    def test_defined(self, value):
+        assert defined(value)

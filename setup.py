496f44966f5ebc93b77e209ef4526500032d2bf3
Aliaksei Urbanski <aliaksei@inspectorio.com>
2018-11-09 16:10:36 +0300

    Improve package versioning
    PEP 396:
    https://www.python.org/dev/peps/pep-0396/
    
    Single-sourcing the package version:
    https://packaging.python.org/guides/single-sourcing-package-version/

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+import os
+import re
+
 from setuptools import find_packages, setup
+
+PACKAGE_NAME = 'acf'
+
+
+def get_version():
+    init_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), PACKAGE_NAME, '__init__.py'
+    )
+    with open(init_path) as init_file:
+        return re.search(
+            r"^__version__ = '(?P<version>.*?)'.*$",
+            init_file.read(), re.MULTILINE
+        ).group('version')
 
 
 setup(
     python_requires='>=2.7',
-    name='acf',
-    version='0.9.0',
+    name=PACKAGE_NAME,
+    version=get_version(),
     description='API Client Framework',
     install_requires=[
         'cached-property>=1.4,<2',
